9131b58f3c1f0564ba408c71b2a5e31650e2d2c1
Radoslaw Grabos <radgra2@gmail.com>
2021-05-02 21:18:09 +0200

    Auto-generated commit

--- src/models/product.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct {
-    ProductID: number;
-    ProductName: string;
-    UnitsInStock: number;
-    UnitsOnOrder: number;
-    UnitPrice: number;
-    SupplierID: number;
-    Discontinued: boolean;
-}
